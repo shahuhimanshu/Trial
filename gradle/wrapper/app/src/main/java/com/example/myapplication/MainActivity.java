46859f4984c30a91da851c7b522c97953dab980d
Himanshu <shahuha9@gmail.com>
2023-07-22 01:07:29 +0530

    First Commit

---
+++ gradle/wrapper/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import kotlin.Metadata;
+
+public final class MainActivity extends AppCompatActivity {
+
+
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+    }
+}
